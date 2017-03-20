22649aa275d007c669a4850bf26d406588620395
Tord <tord.nilsen@gmail.com>
2017-03-20 11:13:25 +0100

    Added getphoto and geofire
    Added getphoto and geofire

--- js/addphoto.js
+++ js/addphoto.js
@@ -6,40 +6,70 @@
     storageBucket: "fotodugnad-c731f.appspot.com",
     messagingSenderId: "696488879347"
 
-  });	
+  });
  function _addphoto(title, description, thumbnail_url, photo_url, lat, lon) {
 
-
-
- 
-
-  // Generate a random Firebase location
-  var firebaseRef = firebase.database().ref().push();
-
-  // Create a new GeoFire instance at the random Firebase location
-  var geoFire = new GeoFire(firebaseRef);
 
   var postData = {
     title: title,
     description: description,
     thumbnail_url: thumbnail_url,
-    photo_url: photo_url,
-    lat: lat,
-    lon:  lon
+    photo_url: photo_url
+
 
   };
-  
+
+
+
+
   // Adding photo to Firebase
 
- var key = firebase.database().ref().child('photos').push().key;
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  var updates = {};
-  updates['/photos/' + key] = postData;
-  return firebase.database().ref().update(updates);
+  var key = firebase.database().ref().child('photos').push().key;
+   // Write the new post's data simultaneously in the photo list and the geofire' list.
 
- 
- 
+   var updates = {};
+   updates['/photos/' + key] = postData;
+   firebase.database().ref().update(updates);
+
+  var firebaseRef = firebase.database().ref('photos').child('_geofire');
+
+   var geoFire = new GeoFire(firebaseRef);
+
+var lat=Number(lat);
+var lon=Number(lon)
+   geoFire.set(key, [lat, lon]);
+log('Added');
+
+
 }
+
+function _getphoto(lat,lon) {
+  // Generate a random Firebase location
+  var firebaseRef = firebase.database().ref('photos').child('_geofire');
+
+var lat=Number(lat);
+var lon=Number(lon);
+
+
+  var geoFire = new GeoFire(firebaseRef);
+  // Create a GeoQuery
+  var geoQuery = geoFire.query({
+    center: [lat,lon],
+    radius: 10
+  });
+
+  // Attach event callbacks to the query
+  var onKeyEnteredRegistration = geoQuery.on("key_entered", function(key, location) {
+    log(key + " entered the query. You are at " + location);
+
+
+});
+
+}
+
+
+
+
 
   /*************/
   /*  HELPERS  */
